31dc39871835eccbe32ec1d6d9c788508e32e30b
Frederik Ar. Mikkelsen <frogkr@gmail.com>
2017-02-24 20:42:55 +0100

    Added source files

---
+++ Source/YoungWizardAdventure/AdventureStateBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameStateBase.h"
+#include "AdventureStateBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class YOUNGWIZARDADVENTURE_API AAdventureStateBase : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
